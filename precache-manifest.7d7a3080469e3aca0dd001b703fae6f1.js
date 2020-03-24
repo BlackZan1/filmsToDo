e87cb11766dbcffc6d4fe8f16ddafb3326088658
N.Saaliev <kenshin.nakamura@mail.ru>
2020-03-24 13:59:46 +0600

    Updates

---
+++ precache-manifest.7d7a3080469e3aca0dd001b703fae6f1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf5a22aafaa755a692af41676c2d41b3",
+    "url": "/filmsToDo/index.html"
+  },
+  {
+    "revision": "b23cb1154528eae972b0",
+    "url": "/filmsToDo/static/css/main.f0462888.chunk.css"
+  },
+  {
+    "revision": "36af064a9769cf20d088",
+    "url": "/filmsToDo/static/js/2.c3653f21.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/filmsToDo/static/js/2.c3653f21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b23cb1154528eae972b0",
+    "url": "/filmsToDo/static/js/main.834cc4ab.chunk.js"
+  },
+  {
+    "revision": "645791c85fdf45a81b11",
+    "url": "/filmsToDo/static/js/runtime-main.214cb57d.js"
+  },
+  {
+    "revision": "1d0a56b480fe48c6c4ed5154c45e3cec",
+    "url": "/filmsToDo/static/media/link.1d0a56b4.cur"
+  },
+  {
+    "revision": "67578e9c3be0b95de26c3475dfee3d68",
+    "url": "/filmsToDo/static/media/loader.67578e9c.gif"
+  },
+  {
+    "revision": "5b09b5e5ead18095f3837333a0588089",
+    "url": "/filmsToDo/static/media/option.5b09b5e5.png"
+  },
+  {
+    "revision": "5794a310ace4631443c9dfb55a85ca3a",
+    "url": "/filmsToDo/static/media/unavailiable.5794a310.cur"
+  }
+]);
