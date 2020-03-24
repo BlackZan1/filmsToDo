57c425406ab6dabc7cbc4d4dbc0289605841b962
N.Saaliev <kenshin.nakamura@mail.ru>
2020-03-24 14:16:33 +0600

    second fix commit yeah!

--- src/api/api.js
+++ src/api/api.js
@@ -3,10 +3,10 @@ import Axios from 'axios';
 const API_KEY = 'b007e99d'
 
 const instance = Axios.create({
-    baseURL: 'www.omdbapi.com/'
+    baseURL: 'http://www.omdbapi.com/'
 })
 
 export const getFilmsByTitle = (title) => {
-    return instance.get(`?s=${title}&apikey=${API_KEY}&page=${1}`) // t - one film, s = range
+    return instance.get(`?s=${title}&apikey=${API_KEY}`) // t - one film, s = range
     .then(res => res.data.Search)
 } 
